0e4b6b723c35799b76907c1d9b0d30476c85778a
Cao Anh Quan <aquancva@gmail.com>
2017-12-09 12:16:19 +0700

    first commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>Hello world !</div>
+            <button onClick={() => console.log("Hi there!")}>Press me!</button>
+        </div>
+    );
+};
+
+export default Home;
