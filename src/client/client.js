97c02abbd51ab93ef0fe274218999d19bdfc8355
quan <aquancva@gmail.com>
2017-12-21 12:33:01 -0500

    up

--- src/client/client.js
+++ src/client/client.js
@@ -3,10 +3,18 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './Routes';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>
-    , document.querySelector("#root"));
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector("#root")
+);
