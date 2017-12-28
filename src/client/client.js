6df394cfa23cb6876d50d2111d5bbad41ad287eb
quan <aquancva@gmail.com>
2017-12-28 11:34:24 -0500

    Add UsersList and babel-polyfill

--- src/client/client.js
+++ src/client/client.js
@@ -1,10 +1,11 @@
 // Startup point for client side application
+import 'babel-polyfill';
 import React from "react";
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './Routes';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
