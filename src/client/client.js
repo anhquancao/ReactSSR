0c5ead701213e38076e128b8febd0e4bc9658565
Cao Anh Quan <aquancva@gmail.com>
2018-01-11 08:56:08 +0700

    proxy

--- src/client/client.js
+++ src/client/client.js
@@ -8,12 +8,17 @@ import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import axios from 'axios';
 import {renderRoutes} from "react-router-config";
+
+const axiosInstance = axios.create({
+    baseURL: "/api"
+});
 
 const store = createStore(
     reducers,
     window.INITIAL_STATE,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk.withExtraArgument(axiosInstance))
 );
 
 ReactDOM.hydrate(
